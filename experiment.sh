e20d5e750609d4a4bafd0f17675024cfea6d8d39
reese <reese@localhost.localdomain>
2018-12-28 01:20:53 +0800

    result2.0

--- experiment.sh
+++ experiment.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
 
-data_path=data_400/
-time_unit=60
-num=20
+data_path=data_3600/
+time_unit=180
+num=60
 
 
 python init_road.py $num $time_unit 
 
 
 
-for((i=1;i<=10;i++));  
+for((i=1;i<=1000;i++));  
 do   
 python predict.py $data_path $time_unit
 
-python RSA.py $data_path 0 19 380 399 210
+python RSA.py $data_path 0 59 3540 3599 1830
 
 cd $data_path
 rm -rf *e.dat *pre_speed.dat
